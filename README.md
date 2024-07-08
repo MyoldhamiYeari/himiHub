f1b3182472915325e37e387a83e367bfb304547e
MyoldhamiYeari <86677916+MyoldhamiYeari@users.noreply.github.com>
2024-07-08 21:45:27 +0200

    Update
    Added more features, fixed up readme.md

--- README.md
+++ README.md
@@ -4,3 +4,11 @@ Free, open source hub made for personal use.
 
 Currently being worked on.
 Code might not be the best, i am still new to this type of scripting.
+
+Huge inspiration from :
+Sirius (was a very good script)
+
+Please keep in mind i have used notification and spatial audio from :
+Sirius
+
+Nothing else was used.
--- script.lua
+++ script.lua
@@ -31,7 +31,15 @@ local himihubvalues = {
 	UIUrl = "https://github.com/MyoldhamiYeari/himiHub/raw/main/himihubui.rbxm",
 	UIid = "18398134908",
 	Folder = "himihub/",
-	ver = "0.1a"
+	ver = "0.13a",
+	barAnims = {
+		Buttons = {
+			Home = {
+				Open = UDim2.new(0, 3, 0.5, 0),
+				Closed = UDim2.new(0, 3, 0.5, -50)
+			}
+		}
+	}
 }
 
 local function checkFolder() 
@@ -50,41 +58,99 @@ local function checkFolder()
 end
 checkFolder()
 
---[[getfenv().error = function() -- Add these so you wouldn't get detected easily. (dont thank me but its bad)
-	return
-end
-getfenv().print = function()
-	return
-end
-getfenv().warn = function()
-	return
-end]]
-
 function getUIandLoad()
-	--[[local data = game:HttpGet(himihubvalues.UIUrl)
-	if not data then
-		return false
-	end
-	writefile(himihubvalues.Folder.."/Assets/UI.rbxm", data)
-	return getcustomasset(himihubvalues.Folder.."/Assets/UI.rbxm")]]
 	return game:GetObjects("rbxassetid://"..himihubvalues.UIid)[1]
 end
 
-local loadingUI = game:GetObjects("rbxassetid://18398146507")[1]
+--[[local loadingUI = game:GetObjects("rbxassetid://18398146507")[1]
 loadingUI.Parent = coreGui
-local loadingText = loadingUI.Frame.TextLabel
--- The loading ui.
+local loadingText = loadingUI.Frame.TextLabel]]
 
 local UI = getUIandLoad()
 if not UI then
-	loadingText.Text = "Failed to load, sorry!"
-	cloneref(game:GetService('Debris')):AddItem(loadingUI, 5)
+	--loadingText.Text = "Failed to load, sorry!"
+	--cloneref(game:GetService('Debris')):AddItem(loadingUI, 5)
 	return
 end
 UI.Parent=coreGui
-loadingUI:Destroy()
+--loadingUI:Destroy()
 
 local notificationContainer = UI.Notifications
+local barContainer = UI.Bar
+local barOpened = false
+local barDebounce = false
+barContainer.Visible = true
+
+-- Roblox kinda removed my UIScale..
+for _, button in pairs(barContainer.Buttons:GetChildren()) do
+	if not button:FindFirstChild"UIScale" then
+		Instance.new("UIScale", button)
+	end
+end
+
+Instance.new("UIScale", barContainer).Scale=0.8
+barContainer.ImageButton.Position = UDim2.new(.5,0,1,25)
+barContainer.Buttons.ClipsDescendants = true
+
+local ES = Enum.EasingStyle
+local ED = Enum.EasingDirection
+
+function openBar()
+	local barAnims = himihubvalues.barAnims
+	local btns = barAnims.Buttons
+	local bar = barContainer
+	local barBtns = bar.Buttons
+	barBtns.Home.Position = btns.Home.Closed
+	barBtns.Home.UIScale.Scale = 0.8
+	tweenService:Create(bar, TweenInfo.new(.3, ES.Quad, ED.In), {Position=UDim2.new(0.5, 0, 0, -25)}):Play();
+	tweenService:Create(barBtns.Home, TweenInfo.new(.5, ES.Quad, ED.InOut), {Position=btns.Home.Open}):Play();
+	tweenService:Create(barBtns.Home.UIScale, TweenInfo.new(.5, ES.Quad, ED.InOut), {Scale=1}):Play();
+	tweenService:Create(bar.ImageButton.fluency_icon, TweenInfo.new(.15, ES.Quad, ED.InOut), {Rotation=180}):Play();
+	tweenService:Create(bar.ImageButton, TweenInfo.new(.3, ES.Quad, ED.InOut), {Position=UDim2.new(0.5,0,1,5)}):Play();
+	task.wait(.25)
+	tweenService:Create(bar.UIScale, TweenInfo.new(.3, ES.Quad, ED.Out), {Scale=1}):Play();
+	tweenService:Create(bar, TweenInfo.new(.3, ES.Quad, ED.Out), {Position=UDim2.new(0.5, 0, 0, 10)}):Play();
+end
+
+function closeBar()
+	local barAnims = himihubvalues.barAnims
+	local btns = barAnims.Buttons
+	local bar = barContainer
+	local barBtns = bar.Buttons
+	barBtns.Home.Position = btns.Home.Open
+	barBtns.Home.UIScale.Scale = 1
+	tweenService:Create(bar, TweenInfo.new(.3, ES.Quad, ED.In), {Position=UDim2.new(0.5, 0, 0, -25)}):Play();
+	tweenService:Create(bar.UIScale, TweenInfo.new(.3, ES.Quad, ED.In), {Scale=0.8}):Play();
+	tweenService:Create(barBtns.Home, TweenInfo.new(.5, ES.Quad, ED.InOut), {Position=btns.Home.Closed}):Play();
+	tweenService:Create(barBtns.Home.UIScale, TweenInfo.new(.5, ES.Quad, ED.InOut), {Scale=0.8}):Play();
+	tweenService:Create(bar.ImageButton.fluency_icon, TweenInfo.new(.15, ES.Quad, ED.InOut), {Rotation=0}):Play();
+	tweenService:Create(bar.ImageButton, TweenInfo.new(.3, ES.Quad, ED.InOut), {Position=UDim2.new(0.5,0,1,25)}):Play();
+	task.wait(0.25)
+	tweenService:Create(bar, TweenInfo.new(.3, ES.Quad, ED.Out), {Position=UDim2.new(0.5, 0, 0, -55)}):Play();
+end
+
+function toggleBar()
+	if barDebounce then
+		return
+	end
+	barDebounce = true
+	if not barOpened then
+		barOpened = true
+		openBar()
+	else
+		barOpened = false
+		closeBar()
+	end
+	barDebounce = false
+end
+
+barContainer.ImageButton.MouseButton1Down:Connect(function()
+	toggleBar()
+end)
+
+function updateBarTime()
+	barContainer.TextLabel.Text = os.date("%H")..":"..os.date("%M")
+end
 
 local function figureNotifications() -- Thanks to sirius for originally creating this notification system.
 	local notificationsSize = 0
@@ -131,22 +197,22 @@ local function queueNotification(Title, Description, Image)
 		newNotification.Icon.Image = 'rbxassetid://'..Image or 0
 		newNotification:TweenPosition(UDim2.new(0.5, 0, 0, newNotification.Size.Y.Offset + 2), "Out", "Quart", 0.9, true)
 		task.wait(0.1)
-		tweenService:Create(newNotification, TweenInfo.new(0.8, Enum.EasingStyle.Exponential), {Size = UDim2.new(0, 320, 0, newNotification.Description.TextBounds.Y + 50)}):Play()
+		tweenService:Create(newNotification, TweenInfo.new(0.8, ES.Exponential), {Size = UDim2.new(0, 320, 0, newNotification.Description.TextBounds.Y + 50)}):Play()
 		task.wait(0.05)
-		tweenService:Create(newNotification, TweenInfo.new(0.8, Enum.EasingStyle.Exponential), {BackgroundTransparency = 0.35}):Play()
-		tweenService:Create(newNotification.UIStroke, TweenInfo.new(0.6, Enum.EasingStyle.Exponential), {Transparency = 0.7}):Play()
+		tweenService:Create(newNotification, TweenInfo.new(0.8, ES.Exponential), {BackgroundTransparency = 0.35}):Play()
+		tweenService:Create(newNotification.UIStroke, TweenInfo.new(0.6, ES.Exponential), {Transparency = 0.7}):Play()
 		task.wait(0.05)
-		tweenService:Create(newNotification.Icon, TweenInfo.new(0.5, Enum.EasingStyle.Exponential), {ImageTransparency = 0}):Play()
+		tweenService:Create(newNotification.Icon, TweenInfo.new(0.5, ES.Exponential), {ImageTransparency = 0}):Play()
 		task.wait(0.04)
-		tweenService:Create(newNotification.Title, TweenInfo.new(0.5, Enum.EasingStyle.Exponential), {TextTransparency = 0}):Play()
+		tweenService:Create(newNotification.Title, TweenInfo.new(0.5, ES.Exponential), {TextTransparency = 0}):Play()
 		task.wait(0.04)
-		tweenService:Create(newNotification.Description, TweenInfo.new(0.5, Enum.EasingStyle.Exponential), {TextTransparency = 0.15}):Play()
-		tweenService:Create(newNotification.Time, TweenInfo.new(0.5, Enum.EasingStyle.Exponential), {TextTransparency = 0.5}):Play()
+		tweenService:Create(newNotification.Description, TweenInfo.new(0.5, ES.Exponential), {TextTransparency = 0.15}):Play()
+		tweenService:Create(newNotification.Time, TweenInfo.new(0.5, ES.Exponential), {TextTransparency = 0.5}):Play()
 		newNotification.Interact.MouseButton1Click:Connect(function()
 			local foundNotification = table.find(notifications, newNotification)
 			if foundNotification then table.remove(notifications, foundNotification) end
 
-			tweenService:Create(newNotification, TweenInfo.new(0.35, Enum.EasingStyle.Quart, Enum.EasingDirection.In), {Position = UDim2.new(1.5, 0, 0, newNotification.Position.Y.Offset)}):Play()
+			tweenService:Create(newNotification, TweenInfo.new(0.35, ES.Quart, ED.In), {Position = UDim2.new(1.5, 0, 0, newNotification.Position.Y.Offset)}):Play()
 
 			task.wait(0.4)
 			newNotification:Destroy()
@@ -158,7 +224,7 @@ local function queueNotification(Title, Description, Image)
 		task.wait(waitTime)
 		local foundNotification = table.find(notifications, newNotification)
 		if foundNotification then table.remove(notifications, foundNotification) end
-		tweenService:Create(newNotification, TweenInfo.new(0.8, Enum.EasingStyle.Quart, Enum.EasingDirection.In), {Position = UDim2.new(1.5, 0, 0, newNotification.Position.Y.Offset)}):Play()
+		tweenService:Create(newNotification, TweenInfo.new(0.8, ES.Quart, ED.In), {Position = UDim2.new(1.5, 0, 0, newNotification.Position.Y.Offset)}):Play()
 		task.wait(1.2)
 		newNotification:Destroy()
 		figureNotifications()
@@ -166,3 +232,7 @@ local function queueNotification(Title, Description, Image)
 end
 local timeTaken = curtick - tick()
 queueNotification(("Loaded in %s seconds"):format(tostring(timeTaken):sub(1,3)), "Successfully loaded the hub, current version is "..himihubvalues.ver.." remember to make an issue in the github repo if you find a bug, thank you!", "14187686429")
+
+runService.Heartbeat:Connect(function()
+	updateBarTime()
+end)
